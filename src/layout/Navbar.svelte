1b84eaa2775f6662e2e9671bd458cbb499b96224
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-18 19:25:23 +0200

    card post edit delete

--- src/layout/Navbar.svelte
+++ src/layout/Navbar.svelte
@@ -4,10 +4,12 @@
 
 <nav>
     <div class="nav-wrapper">
-      <Link to ="/">Svelte Tutorial</Link>
+      <div class="container">
+        <Link to ="/"><span class="brand-logo">Svelte Tutorial</span></Link>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
       </ul>
+      </div>
     </div>
   </nav>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,1 +1,56 @@
-<h1>Home</h1>
+<script>
+
+import { onMount } from 'svelte';
+import App from "../App.svelte";
+import About from"./about.svelte";
+
+
+    const apiBaseUrl= 'https://jsonplaceholder.typicode.com/posts';
+    let posts = [];
+
+    onMount( async () => {
+        const response = await fetch(apiBaseUrl);
+        posts = await response.json();
+    })
+
+    function editPost(post){
+        console.log(post);
+    }
+
+    function deletePost(){
+        console.log("Deleting post with id: ", id);
+    }
+    
+</script>
+
+<style>
+    .delete-btn{
+        color: red !important;
+    }
+    .card .card-content .card-title{
+        margin-bottom: 0;
+        color: #9999;
+        margin-bottom: 10px;
+    }
+</style>
+
+<div class="row">
+    {#if posts.length === 0}
+    <h3>Loading posts...</h3>
+    {:else}
+        {#each posts as post}
+            <div class="col s6">
+                <div class="card">
+                    <div class="card-content">
+                        <p class="card-title">{post.title}</p>
+                        <p>{post.userId}</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#" on:click={ () => editPost(post) }> Edit</a>
+                        <a href="#" class="delete-btn" on:click={() => deletePost(post.id)}>Delete</a>
+                    </div>
+                </div>
+            </div>
+            {/each}
+            {/if}
+</div>
